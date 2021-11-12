255a06513885b5b675834f4fa314a722aa3fd305
Michael Ørregaard Andersen <michael@initiva.se>
2021-11-12 13:47:27 +0100

    Adding API call to the server

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const path = require('path');
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const pa11y = require('pa11y');
 
 app.use(helmet.hidePoweredBy({ setTo: 'WCAG Compliance Checker 1.0' }));
 app.use(helmet.frameguard());
 app.use(helmet.xssFilter());
 app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -26,6 +32,24 @@ app.use((req, res, next) => {
 
 app.get("/", function(req, res) {
     res.render("index.html");
+});
+
+app.post("/runwcag", function(req, res) {
+    let url = req.body.obj.url;
+    if (url && url.length >= 1) {
+        pa11y(url).then((results) => {
+            res.send({
+                status: "Success",
+                message: "",
+                results: results
+            });
+        });
+    } else {
+      res.send({
+        status: "Error", 
+        message: "No url was sent with the form"
+      })
+    }
 });
 
 server.listen(port, () => console.log(`WCAG Compliance Checker - Running`));
--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,61 @@
 <script lang="ts">
 	let sQ: string = "";
+	let r: any = [];
+	let loading: boolean = false;
 
 	async function handleUrlSubmission(e: any): Promise<void> {
 		e.preventDefault();
-		console.log(sQ);
+		loading = true;
+		
+		const apiResp: Response = await fetch('/runwcag', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ "obj": {
+                "url" : sQ
+            }})
+		});
+		const apiResp_JSON: any = await apiResp.json();
+
+		if (apiResp_JSON.status === "Success") {
+			r = apiResp_JSON;
+			loading = false;
+		} else {
+
+		}
+		
+		console.log(r);
 	}
 </script>
 
 <main class="WCC_Main">
-	<div class="WCC_Row">
+	<section class="WCC_Row">
 		<div class="WCC_SearchContainer">
 			<h1 class="WCC_Headline1">WCAG Compliance Checker</h1>
 			<form class="WCC_SearchForm" on:submit={handleUrlSubmission}>
 				<div class="WCC_SearchForm_Row">
 					<label class="WCC_Label" for="WCC_SearchQuery">Url address</label>
-					<input type="url" class="WCC_Input" bind:value={sQ} autocapitalize="off" autocomplete="off" />
+					<input type="url" id="WCC_SearchQuery" class="WCC_Input" bind:value={sQ} autocapitalize="off" autocomplete="off" />
 				</div>
-				<button type="submit" class="WCC_SubmitButton">Submit</button>
+				<div class="WCC_SearchForm_Row">
+					{#if loading}
+						<span>Loading...</span>
+					{/if}
+					<button type="submit" class="WCC_SubmitButton">Submit</button>
+				</div>
 			</form>
 		</div>
-	</div>
-	<div class="WCC_Row">
-		<div class="WCC_SearchResults"></div>
-	</div>
+	</section>
+	{#if r?.results?.issues?.length >= 1}
+		<section class="WCC_Row">
+			<div class="WCC_SearchResults">
+				<ul>
+					{#each r.results.issues as o}
+						<li>
+							<span>{o.code}</span>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</section>
+	{/if}
 </main>
