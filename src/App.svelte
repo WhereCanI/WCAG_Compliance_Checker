a46be701e4ccb337b91b9abe7ef33e7ea7d2f633
Michael Ørregaard Andersen <michael@initiva.se>
2021-11-12 09:44:13 +0100

    Adding basic HTML URL Search Query

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
-	// CREATE CODE HERE
+	let sQ: string = "";
+
+	async function handleUrlSubmission(e: any): Promise<void> {
+		e.preventDefault();
+		console.log(sQ);
+	}
 </script>
 
-<main>
-	
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="WCC_Main">
+	<div class="WCC_Row">
+		<div class="WCC_SearchContainer">
+			<h1 class="WCC_Headline1">WCAG Compliance Checker</h1>
+			<form class="WCC_SearchForm" on:submit={handleUrlSubmission}>
+				<div class="WCC_SearchForm_Row">
+					<label class="WCC_Label" for="WCC_SearchQuery">Url address</label>
+					<input type="url" class="WCC_Input" bind:value={sQ} autocapitalize="off" autocomplete="off" />
+				</div>
+				<button type="submit" class="WCC_SubmitButton">Submit</button>
+			</form>
+		</div>
+	</div>
+	<div class="WCC_Row">
+		<div class="WCC_SearchResults"></div>
+	</div>
 </main>
